779feab2abf1c0f90863455e63ae6d936aca4747
侯志军 <iosdeveloper@iOSdeMac-mini.local>
2016-08-01 15:13:12 +0800

    task-1
    测试的东西

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by iOS developer on 16/7/29.
+//  Copyright © 2016年 iOS developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by iOS developer on 16/7/29.
+//  Copyright © 2016年 iOS developer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
